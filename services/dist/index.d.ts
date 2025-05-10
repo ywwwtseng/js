7652136f3c0a6e57bc2235c2f2523cb0146a9c67
William <ywwwtseng@gmail.com>
2025-05-11 00:22:46 +0800

    feat: mutation service

---
+++ services/dist/index.d.ts
@@ -0,0 +1,13 @@
+import type { DeepPartial } from '@libs/types';
+export type Action<TState = any, TTable = any> = {
+    '@table'?: TTable;
+    name: string;
+    validate?: (params?: unknown) => boolean;
+    state?: (state: Partial<TState>) => {
+        path: string[];
+        value: unknown;
+    };
+    execute?: (state: DeepPartial<TState> | undefined) => void;
+    log?: boolean;
+    effect?: (state: DeepPartial<TState>) => void;
+};
